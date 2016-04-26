12f7df3b641a6963743b2f7d5117c2084c511765
Zorox3 <timogoe@gmail.com>
2016-04-26 10:38:34 +0200

    CraftingManager added

---
+++ src/main/java/de/atomicpower/Main/CraftingManager.java
@@ -0,0 +1,22 @@
+package de.atomicpower.Main;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class CraftingManager {
+
+	public void init(){
+		initCraftingRecipies();
+		intiSmeltingRecipies();
+	}
+
+	private void intiSmeltingRecipies() {
+		
+		
+		
+	}
+
+	private void initCraftingRecipies() {
+		
+	}
+	
+}
